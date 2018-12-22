7129a1ebfe63bd19ca2cc25f454345c7f0c65c82
Clark Newell <williamcnewell@gmail.com>
2018-12-21 21:11:36 -0700

    daily memo

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,16 +1,26 @@
 // FizzBuzz
 
-// Dissection: run a for loop, iterate 
-for ( i = 0; i <  101; i++ ) {
+// Dissection: run a for loop 100 times
+// if i is divisible by 15 with no remainder, print "FizzBuzz"
+// else if  
+
+for ( i = 1; i <  101; i++ ) {
     if ( i % 15 == 0) console.log('FizzBuzz')
     else if ( i % 3 == 0) console.log('Fizz')
     else if ( i % 5 == 0) console.log('Buzz')
     else console.log(i)
 }
 
-for ( i = 0; i < 101; i++ ) {
+for ( i = 1; i < 101; i++ ) {
     if ( i % 15 == 0) console.log('FizzBuzz')
     else if ( i % 3 == 0) console.log('Fizz')
     else if ( i % 3 == 0) console.log('Buzz')
+    else console.log(i)
+}
+
+for ( i = 1; i < 101; i++ ) {
+    if ( i % 15 == 0 ) console.log('FizzBuzz')
+    if ( i % 3 == 0 ) console.log('Fizz')
+    if ( i % 5 == 0 ) console.log('Buzz')
     else console.log(i)
 }
