8ef0f6ba43354fc08225d8be940bcb60bd431ad4
Clark Newell <williamcnewell@gmail.com>
2018-12-22 09:06:52 -0700

    daily memo

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -24,3 +24,10 @@ for ( i = 1; i < 101; i++ ) {
     if ( i % 5 == 0 ) console.log('Buzz')
     else console.log(i)
 }
+
+for ( i = 1; i < 101; i++ ) {
+    if ( i % 15 == 0 ) console.log('FizzBuzz')
+    if ( i % 3 == 0 ) console.log('Fizz')
+    if ( i % 4 == 0 ) console.log('Buzz')
+    else console.log(i)
+}
