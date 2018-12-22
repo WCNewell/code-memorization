5161177d365080afb6a4146bf8a85660c62a203b
Clark Newell <williamcnewell@gmail.com>
2018-12-22 09:20:14 -0700

    new daily memo

---
+++ ConvertToUrl.js
@@ -0,0 +1,24 @@
+// Convert to URL or "URLify a given string"
+
+// Convert a string into an acceptable URL by converting all spaces into %20
+// Side note: URI is Uniform Resource Identifier, a URL (Uniform Resource Locator) is the most common form of URI
+
+var newStr = 'Mr. Smith goes to Washington'
+
+var newNewStr = 'Pluto IS a planet'
+
+// Input: string.string.length<10^5
+// Output: new string formatted as a URL
+
+// PsuedoCode: Write a for loop to iterate over string (function input)
+// Find and replace blank space with '%20'
+// Use .replace method
+
+function convertToUrl(string, target, replacement) {
+    for ( i = 0; i < string.length; i++ ) {
+        string = string.replace(target, replacement);
+    }
+    return string;
+}
+
+console.log(convertToUrl(newStr, " ", "%20"))
