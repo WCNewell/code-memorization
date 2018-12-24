10eec89edfd75f7b4b75b7fc662fe51e71c92433
Clark Newell <williamcnewell@gmail.com>
2018-12-24 10:57:51 -0700

    daily memo

---
+++ ToUpperCase.js
@@ -0,0 +1,32 @@
+// ToUpperCase
+// Write a function that takes a string and returns the uppercase version of that string with an '!' at the end
+
+let stringOne = 'Bobbicat says meow'
+
+function upperCase(string) {
+    const upperString = string.toUpperCase()
+    return `${upperString}`
+}
+
+console.log(upperCase(stringOne))
+
+function upperCase(string) {
+    const upperString = string.toUpperCase()
+    return `${upperString}`
+}
+
+console.log(upperCase(stringOne))
+
+function upperCase(string) {
+    const upperString = string.toUpperCase()
+    return `${upperString}`
+}
+
+console.log(upperCase(stringOne))
+
+function upperCase(string) {
+    const upperString = string.toUpperCase()
+    return `${upperString}`
+}
+
+console.log(upperCase(stringOne))
