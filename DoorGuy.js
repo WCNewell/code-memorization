3fc8841a5d0ed32ac385a438e38a35679f214531
Clark Newell <williamcnewell@gmail.com>
2018-12-26 14:24:14 -0700

    daily memo

--- DoorGuy.js
+++ DoorGuy.js
@@ -2,9 +2,9 @@
 // Takes an array of years and returns an array of booleans stating whether or not a person is of legal drinking age
 
 let years = [2001, 1973, 1981, 1991, 2014]
-let results = []
 
 function canDrink(yearsArray) {
+    let results = []
     for (i = 0; i < yearsArray.length; i++) {
         let age = 2018 - yearsArray[i]
         if (age >= 21) {
@@ -19,6 +19,7 @@ function canDrink(yearsArray) {
 console.log(canDrink(years))
 
 function canDrink(yearsArray) {
+    let results = []
     for ( i = 0; i < yearsArray.length; i++ ) {
         let age = 2018 - yearsArray[i]
         if (age > 21) {
@@ -30,3 +31,49 @@ function canDrink(yearsArray) {
     return results
 }
 
+console.log(canDrink(years))
+
+function canDrink(yearsArray) {
+    let results = []
+    for ( i = 0; i < yearsArray.length; i++ ) {
+        let age = 2018 - yearsArray[i]
+        if (age > 21) {
+            results.push(true)    
+        } else {
+            results.push(false)
+        }
+    }
+    return results
+}
+
+console.log(canDrink(years))
+
+function canDrink(yearsArray) {
+    let results = []
+    for ( i = 0; i < yearsArray.length; i++ ) {
+        let age = 2018 - yearsArray[i]
+        if (age > 21) {
+            results.push(true)
+        } else {
+            results.push(false)
+        }
+    }
+    return results
+}
+
+console.log(canDrink(years))
+
+function canDrink(yearsArray) {
+    let results = []
+    for ( i = 0; i < yearsArray.length; i++ ) {
+        let age = 2018 - yearsArray[i]
+        if (age > 21) {
+            results.push(true)
+        } else {
+            results.push(false)
+        }
+    }
+    return results
+}
+
+console.log(canDrink(years))
