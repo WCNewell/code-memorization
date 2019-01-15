fde4750546dc62181a15b6513c61a892508af4ac
Clark Newell <williamcnewell@gmail.com>
2019-01-14 18:08:28 -0700

    daily memo

--- MergedStrings.js
+++ MergedStrings.js
@@ -15,3 +15,18 @@ function mergeStrings(str1, str2) {
 }
 
 console.log(mergeStrings(str1, str2))
+
+function mergeStrings(str1, str2) {
+    let a = str1.split("").filter(Boolean)
+    let b = str2.split("")
+
+    let mergedString = ''
+
+    for (i = 0; i < a.length; i++ ) {
+        if (i < a.length) mergedString += a[i]
+        if (i < b.length) mergedString += b[i]
+    }
+    return mergedString
+}
+
+console.log(mergeStrings(str1, str2))
