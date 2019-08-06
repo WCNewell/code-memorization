80ee53f6d1be57483634397ed6adbc706439d4a9
W. Clark Newell <clark@clarknewell.tech>
2019-08-06 16:46:17 -0600

    daily code memo

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -4,51 +4,58 @@
 // if i is divisible by 15 with no remainder, print "FizzBuzz"
 // else if  
 
-for ( i = 1; i <  101; i++ ) {
-    if ( i % 15 == 0) console.log('FizzBuzz')
-    else if ( i % 3 == 0) console.log('Fizz')
-    else if ( i % 5 == 0) console.log('Buzz')
-    else console.log(i)
-}
+// for ( i = 1; i <  101; i++ ) {
+//     if ( i % 15 == 0) console.log('FizzBuzz')
+//     else if ( i % 3 == 0) console.log('Fizz')
+//     else if ( i % 5 == 0) console.log('Buzz')
+//     else console.log(i)
+// }
 
-for ( i = 1; i < 101; i++ ) {
-    if ( i % 15 == 0) console.log('FizzBuzz')
-    else if ( i % 3 == 0) console.log('Fizz')
-    else if ( i % 3 == 0) console.log('Buzz')
-    else console.log(i)
-}
+// for ( i = 1; i < 101; i++ ) {
+//     if ( i % 15 == 0) console.log('FizzBuzz')
+//     else if ( i % 3 == 0) console.log('Fizz')
+//     else if ( i % 3 == 0) console.log('Buzz')
+//     else console.log(i)
+// }
 
-for ( i = 1; i < 101; i++ ) {
-    if ( i % 15 == 0 ) console.log('FizzBuzz')
-    if ( i % 3 == 0 ) console.log('Fizz')
-    if ( i % 5 == 0 ) console.log('Buzz')
-    else console.log(i)
-}
+// for ( i = 1; i < 101; i++ ) {
+//     if ( i % 15 == 0 ) console.log('FizzBuzz')
+//     if ( i % 3 == 0 ) console.log('Fizz')
+//     if ( i % 5 == 0 ) console.log('Buzz')
+//     else console.log(i)
+// }
 
-for ( i = 1; i < 101; i++ ) {
-    if ( i % 15 == 0 ) console.log('FizzBuzz')
-    if ( i % 3 == 0 ) console.log('Fizz')
-    if ( i % 4 == 0 ) console.log('Buzz')
-    else console.log(i)
-}
+// for ( i = 1; i < 101; i++ ) {
+//     if ( i % 15 == 0 ) console.log('FizzBuzz')
+//     if ( i % 3 == 0 ) console.log('Fizz')
+//     if ( i % 4 == 0 ) console.log('Buzz')
+//     else console.log(i)
+// }
 
-for ( i = 1; i < 101; i++ ) {
-    if ( i % 15 == 0 ) console.log('FizzBuzz')
-    else if ( i % 5 == 0 ) console.log('Buzz')
-    else if ( i % 3 == 0 ) console.log('Fizz')
-    else console.log(i)
-}
+// for ( i = 1; i < 101; i++ ) {
+//     if ( i % 15 == 0 ) console.log('FizzBuzz')
+//     else if ( i % 5 == 0 ) console.log('Buzz')
+//     else if ( i % 3 == 0 ) console.log('Fizz')
+//     else console.log(i)
+// }
+
+// for ( i = 1; i < 101; i++ ) {
+//     if ( i % 15 == 0 ) console.log('FizzBuzz')
+//     else if ( i % 5 == 0 ) console.log('Buzz')
+//     else if ( i % 3 == 0 ) console.log('Fizz')
+//     else console.log(i) 
+// }
+
+// for ( i = 1; i < 101; i++ ) {
+//     if ( i % 15 == 0 ) console.log('FizzBuzz')
+//     else if ( i % 5 == 0) console.log('Buzz')
+//     else if ( i % 3 == 0) console.log('Fizz')
+//     else console.log(i)
+// }
 
 for ( i = 1; i < 101; i++ ) {
     if ( i % 15 == 0 ) console.log('FizzBuzz')
     else if ( i % 5 == 0 ) console.log('Buzz')
     else if ( i % 3 == 0 ) console.log('Fizz')
     else console.log(i) 
-}
-
-for ( i = 1; i < 101; i++ ) {
-    if ( i % 15 == 0 ) console.log('FizzBuzz')
-    else if ( i % 5 == 0) console.log('Buzz')
-    else if ( i % 3 == 0) console.log('Fizz')
-    else console.log(i)
 }
