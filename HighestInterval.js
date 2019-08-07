306872b7d8ecac874d0ab84523341917d7ccc111
W. Clark Newell <clark@clarknewell.tech>
2019-08-07 16:19:30 -0600

    daily code memo

--- HighestInterval.js
+++ HighestInterval.js
@@ -89,3 +89,17 @@ function highestInterval(integers) {
 }
 
 console.log(highestInterval(integers))
+
+function highestInterval(integers) {
+    let highestInt = null;
+    for ( x = 0; x < integers.length; x++) {
+        for (y = 0; y < integers.length; y++) {
+            if (integers[x] < integers[y] && highestInt < (integers[y] - integers[x])) {
+                highestInt = integers[y] - integers[x]
+            }
+        }
+    }
+    return highestInt === null ? -1 : highestInt
+}
+
+console.log(highestInterval(integers))
