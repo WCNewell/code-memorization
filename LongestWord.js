f5e17c36c582f9a956ce3336cb5fbf28566e93cc
W. Clark Newell <clark@clarknewell.tech>
2020-01-08 14:26:19 -0700

    daily code memo

---
+++ LongestWord.js
@@ -0,0 +1,26 @@
+// LongestWord
+// Find the longest word in a string
+
+// let str ="Bobbicat is super cute"
+
+function findLongestWord(str) {
+  var strSplit = str.split(' ');
+  var longestWord = 0;
+  for(var i = 0; i < strSplit.length; i++){
+    if(strSplit[i].length > longestWord){
+	longestWord = strSplit[i].length;
+     }
+  }
+  return longestWord;
+}
+
+findLongestWord("Bobbicat is super cute")
+
+// or 
+
+function findLongestWord(str) {
+  var longestWord = str.split(' ').sort(function(a, b) { return b.length - a.length; });
+  return longestWord[0].length;
+}
+
+console.log(findLongestWord(str))
